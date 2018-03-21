9fd824aace7eb8c821ee1210c7f5bd02ca514f2c
linh8911nt <thanhlinh12314@gmail.com>
2018-03-21 11:52:48 +0700

    pf-java-students-pass-exam

---
+++ StudentsPassExam.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class StudentsPassExam {
+    public static void main(String[] args) {
+        int size;
+        int[] students;
+        Scanner input = new Scanner(System.in);
+
+        //Kiểm tra số lượng sinh viên nhập vào không quá 30 người
+        do {
+            System.out.println("Số lượng sinh viên: ");
+            size = input.nextInt();
+            if (size > 30)
+                System.out.println("Số lượng sinh viên không quá 30 người");
+        }while (size > 30);
+
+        //Khởi tạo danh sách
+        students = new int[size];
+
+        //Nhập điểm sinh viên vào danh sách
+        for (int i = 0; i < students.length; i++){
+            System.out.println("Điểm sinh viên " + (i + 1) + " : ");
+            students[i] = input.nextInt();
+        }
+
+        //in ra danh sách sinh viên và đếm số lượng sinh viên thi đỗ
+        int count = 0;
+        for (int j = 0; j < students.length; j++){
+            System.out.println("Điểm: " + students[j]);
+            if (students[j] > 5 && students[j] <= 10)
+                count += 1;
+        }
+        System.out.println("Số lượng sinh viên đỗ: " + count);
+    }
+}
